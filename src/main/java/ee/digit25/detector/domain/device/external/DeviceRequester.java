0b16ed7b178e04d044bb856db362b374a044c51a
ERICH JAGOMAGIS <erich.jagomagis@bitweb.ee>
2025-05-06 09:39:56 +0300

    Added additional endpoint integrations

--- src/main/java/ee/digit25/detector/domain/device/external/DeviceRequester.java
+++ src/main/java/ee/digit25/detector/domain/device/external/DeviceRequester.java
@@ -8,17 +8,31 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DeviceRequester {
 
-    private final DeviceApi deviceApi;
+    private final DeviceApi api;
     private final DeviceApiProperties properties;
 
     public Device get(String mac) {
         log.info("Requesting device with mac({})", mac);
 
-        return RetrofitRequestExecutor.executeRaw(deviceApi.get(properties.getToken(), mac));
+        return RetrofitRequestExecutor.executeRaw(api.get(properties.getToken(), mac));
+    }
+
+    public List<Device> get(List<String> macs) {
+        log.info("Requesting devices with macs {}", macs);
+
+        return RetrofitRequestExecutor.executeRaw(api.get(properties.getToken(), macs));
+    }
+
+    public List<Device> get(int pageNumber, int pageSize) {
+        log.info("Requesting persons page {} of size {}", pageNumber, pageSize);
+
+        return RetrofitRequestExecutor.executeRaw(api.get(properties.getToken(), pageNumber, pageSize));
     }
 }
--- src/main/java/ee/digit25/detector/domain/device/external/api/DeviceApi.java
+++ src/main/java/ee/digit25/detector/domain/device/external/api/DeviceApi.java
@@ -1,12 +1,18 @@
 package ee.digit25.detector.domain.device.external.api;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
-import retrofit2.http.Path;
+import retrofit2.http.*;
+
+import java.util.List;
 
 public interface DeviceApi {
 
     @GET("/devices/{mac}")
     Call<Device> get(@Header("Authorization") String token, @Path("mac") String mac);
+
+    @POST("/devices/by-macs")
+    Call<List<Device>> get(@Header("Authorization") String token, @Body List<String> macs);
+
+    @GET("/devices")
+    Call<List<Device>> get(@Header("Authorization") String token, @Query("pageNumber") int pageNumber, @Query("pageSize") int pageSize);
 }
